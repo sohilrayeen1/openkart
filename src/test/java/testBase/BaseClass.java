b4be7e7593d277d1474cac7c756e95b6079723b3
sohil <sohilkhanraghogarh@gmail.com>
2023-10-13 00:44:51 +0530

    second

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -9,19 +9,17 @@ import java.util.ResourceBundle;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;  // logging
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;  // logging
 
 public class BaseClass {
 
@@ -45,6 +43,9 @@ public class BaseClass {
 		
 		if(br.equals("chrome"))
 		{
+			System.setProperty("webdriver.http.factory", "jdk-http-client");
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\Hp\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		driver=new ChromeDriver();
 		}
 		else if(br.equals("edge"))
---
+++ src/test/java/testCases/aaa.java
@@ -0,0 +1,12 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class aaa {
+
+	@Test
+	void t() {
+		
+	}
+	
+}
